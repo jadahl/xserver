0259da8d11dcc96512d813bec6b3d689e1f3a575
Krzysztof Sobiecki <sobkas@gmail.com>
2016-04-20 12:56:47 +0200

    xwayland: Add a new input device used for pointer warping/locking
    Generating relative and absolute movement events from the same input
    device is problematic, because an absolute pointer device doesn't
    expect to see any relative motion events. To be able to generate
    relative pointer motion events including unaccelerated deltas, create a
    secondary pointer device 'xwayland-relative-pointer', and use that for
    emitting relative motion events.
    
    Signed-off-by: Krzysztof Sobiecki <sobkas@gmail.com>
    Signed-off-by: Jonas Ådahl <jadahl@gmail.com>

--- hw/xwayland/xwayland.h
+++ hw/xwayland/xwayland.h
@@ -119,6 +119,7 @@ struct xwl_touch {
 
 struct xwl_seat {
     DeviceIntPtr pointer;
+    DeviceIntPtr relative_pointer;
     DeviceIntPtr keyboard;
     DeviceIntPtr touch;
     struct xwl_screen *xwl_screen;
